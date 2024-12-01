4ad2e7c7d15cb633103e5e3a3507209cef1be26e
Phongdzfk <117740087+Phongdzfk@users.noreply.github.com>
2024-12-01 14:02:10 +0700

    Update Comment.java

--- DataClass/Summary_Classes/Comment.java
+++ DataClass/Summary_Classes/Comment.java
@@ -1,65 +1,38 @@
-package Summary_Classes;
-import java.sql.*;
-public class Comment {
-    private String description, image, author, materialId, status, parentId;
-    private Timestamp createAt, deleteAt;
-    public Comment(String description, String image, String author, String materialId, String parentId) {
-        this.description = description;
-        this.image = image;
-        this.author = author;
-        this.materialId = materialId;
-        this.parentId = parentId;
-        this.status = "active";
-        this.createAt = new Timestamp(System.currentTimeMillis());
-    }
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
 
-    public void deleteComment(){
-        this.status = "inactive";
-        this.deleteAt = new Timestamp(System.currentTimeMillis());
-    }
+import java.io.Serializable;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getMaterialId() {
-        return materialId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getParentId() {
-        return parentId;
-    }
-
-    public Timestamp getCreateAt() {
-        return createAt;
-    }
-
-    public Timestamp getDeleteAt() {
-        return deleteAt;
+/**
+ *
+ * @author 24hph
+ */
+public class Comment implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private User author; // Store username instead of User object
+    private String content;
+    private String timestamp;
+    
+    public Comment(User authorUsername, String content, String timestamp) {
+        this.author = authorUsername;
+        this.content = content;
+        this.timestamp = timestamp;
     }
     
+    public User getAuthorUsername() {
+        return author;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public String getTimestamp() {
+        return timestamp;
+    }
 }
+
