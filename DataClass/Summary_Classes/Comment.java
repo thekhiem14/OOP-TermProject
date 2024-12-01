736dcbc524a37d191a7c175983e80bb7ff47bff2
Dang Tuan Phong <117740087+Phongdzfk@users.noreply.github.com>
2024-12-01 23:54:11 +0700

    Update Final

--- DataClass/Summary_Classes/Comment.java
+++ DataClass/Summary_Classes/Comment.java
@@ -4,10 +4,6 @@ package Summary_Classes;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
-/**
- *
- * @author 24hph
- */
 public class Comment implements Serializable {
     private static final long serialVersionUID = 1L;
     
@@ -31,6 +27,25 @@ public class Comment implements Serializable {
     
     public Timestamp getTimestamp() {
         return timestamp;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;  
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;  
+        }
+        Comment comment = (Comment) obj;  
+        return author.equals(comment.author) && content.equals(comment.content);  
+    }
+
+    @Override
+    public int hashCode() {
+        int result = author.hashCode();  
+        result = 31 * result + content.hashCode(); 
+        return result;
     }
 }
 
