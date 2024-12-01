88f8148813edddb7c5182778740e5eaa061a23cc
Dang Tuan Phong <117740087+Phongdzfk@users.noreply.github.com>
2024-12-02 00:02:23 +0700

    Update Final

--- DataClass/Summary_Classes/User.java
+++ DataClass/Summary_Classes/User.java
@@ -4,10 +4,6 @@ package Summary_Classes;
 import java.io.Serial;
 import java.io.Serializable;
 
-/**
- *
- * @author 24hph
- */
 public abstract class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -41,7 +37,32 @@ public abstract class User implements Serializable {
     {
         return name;
     }
-
+    
+    public void addMaterial(Category currentCategory, Material newMaterial)
+    {
+        CategoryManager categoryManager = new CategoryManager();
+        currentCategory.addMaterial(newMaterial);
+        categoryManager.saveCategories();
+    }
+    public void addCommentToMaterial(Comment newComment,Material material)
+    {
+        material.addComments(newComment);
+    }
+    
+    public void deleteCommentInMaterial(Comment selectedComment, Material material)
+    {
+        material.deleteComments(selectedComment);
+    }
+    
+    public void addRatingToMaterial(Rating newRating, Material material)
+    {
+        material.addRating(newRating);
+    }
+    
+    public void deleteRatingInMaterial(Rating yourRating, Material material)
+    {
+        material.deleteRating(yourRating);
+    }
     // Abstract login method
     public abstract boolean login(String password);
 }
